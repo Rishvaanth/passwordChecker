af31b16ceb7d1ce0c2513393acdffe1a600faf14
Rishvaanth <rishvaantht1998@gmail.com>
2022-10-07 01:04:07 -0500

    Initial commit Completed code Added comments to passwordChecker.py

--- passwordChecker.py
+++ passwordChecker.py
@@ -4,15 +4,15 @@ password = input("Enter the password:")
 
 flag = True
 while True:
-    if len(password) < 8:
+    if len(password) < 8:    # Checking length
         flag = False
         print(f"Password length is too short. Try a longer password")
         break
-    elif not re.search("[A-Z]", password):
+    elif not re.search("[A-Z]", password):  # Checking presence of capital letters
         flag = False
         print(f"No capital letter found. Invalid password")
         break
-    elif not re.search("[0-9]", password):
+    elif not re.search("[0-9]", password):   # Checking if numeric value is present
         flag = False
         print(f"No numeric character found in the password. Thus invalid.")
         break
